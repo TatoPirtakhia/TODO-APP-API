a570a5e6472987babbcd495ae2e1916bd1fbab6c
TatoPirtakhia <tarassatato@gmail.com>
2023-04-24 13:03:22 +0400

    fix: getData

--- src/controllers/project-controller.js
+++ src/controllers/project-controller.js
@@ -2,7 +2,13 @@ import Project from "../models/Project.js"
 
 export const getAllProject = async (req,res) =>{
     const data = await Project.find()
-
-    return res.status(200).json(data)
+    const newData = data.map(data => {
+        return{ 
+            title: data.title,
+            id: data.id,
+            active: data.active
+        }
+    })
+    return res.status(200).json(newData)
 }
 
